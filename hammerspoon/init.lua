62131fee0c23acc87e81032866578f358e92e170
Leroy Nas <leroynas@liberosoftware.com>
2024-01-15 11:36:48 +0100

    master fix: remove auto lock

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,7 +2,7 @@ hs.hotkey.bind({'ctrl'}, '`', nil, function()
     hs.reload()
 end)
 
-require('keyboard.auto-lock')
+-- require('keyboard.auto-lock')
 require('keyboard.dark-mode')
 require('keyboard.hyper')
 require('keyboard.stand')
